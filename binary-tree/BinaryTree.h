456699cc53fd22cee03f2060def839dc32564f01
Eric Huber <echuber2@illinois.edu>
2019-09-24 06:47:59 -0500

    commenting

--- binary-tree/BinaryTree.h
+++ binary-tree/BinaryTree.h
@@ -15,6 +15,7 @@ class BinaryTree {
   private:
     class TreeNode {
       public:
+        // *See note below about how references are being used here.
         T & data;
         TreeNode *left, *right;
         TreeNode(T & data) : data(data), left(nullptr), right(nullptr) { }
@@ -22,6 +23,13 @@ class BinaryTree {
 
     TreeNode *root_;
 };
+
+// *Note that this implementation of a tree is storing explicit aliases
+// to memory using references. This means that the actual allocation,
+// cleanup, and general responsibility for that data memory will lie
+// outside of this class implementation. However, the tree nodes
+// themselves belong to the tree and will need to be created or
+// deleted by the tree.
 
 // Sometimes, your header files might include another header file with
 // further templated definitions. The .h and .hpp are both just filename
